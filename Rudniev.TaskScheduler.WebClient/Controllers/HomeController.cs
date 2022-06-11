f337a95fc1b8e2a9a8dc6ad6d7e6ce9e64edd033
Vladyslav Rudniev <rudniev.csdev@gmail.com>
2022-06-11 08:59:12 +0300

    Add project files.

---
+++ Rudniev.TaskScheduler.WebClient/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rudniev.TaskScheduler.WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
