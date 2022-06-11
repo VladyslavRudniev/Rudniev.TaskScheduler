03688611d7386dde853aaa5405013f066a099528
Vladyslav Rudniev <rudniev.csdev@gmail.com>
2022-06-11 21:33:34 +0300

    Add registration page view and controller. Add RegistrationAndLoginPageStyle.css. Add RegistrationViewModel.cs

---
+++ Rudniev.TaskScheduler.WebClient/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rudniev.TaskScheduler.WebClient.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Rudniev.TaskScheduler.WebClient/Models/RegistrationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rudniev.TaskScheduler.WebClient.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string ReturnUrl { get; set; }
+    }
+}
