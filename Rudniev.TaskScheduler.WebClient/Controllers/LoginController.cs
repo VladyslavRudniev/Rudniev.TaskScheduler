dd8dca2d7a96a4435c4e3ed4a1e8efba06d46c65
Vladyslav Rudniev <rudniev.csdev@gmail.com>
2022-06-12 09:16:51 +0300

    Add login controller and view.

---
+++ Rudniev.TaskScheduler.WebClient/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rudniev.TaskScheduler.WebClient.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
